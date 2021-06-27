c1a78e730c7a3703bd9dd7be7286f9877c0feac3
AnjuMeleth <46922680+AnjuMeleth@users.noreply.github.com>
2021-06-27 16:49:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+# list of cat images
+images = [
+    "https://media.giphy.com/media/33OrjzUFwkwEg/giphy.gif",
+    "https://media.giphy.com/media/v6aOjy0Qo1fIA/giphy.gif"
+        ]
+
+@app.route('/')
+def index():
+    url = random.choice(images)
+    return render_template('index.html', url=url)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
